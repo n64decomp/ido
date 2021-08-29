9f8562d63bf0ffd78a3c520f31f64566a4cd9103
Louis <35883445+louist103@users.noreply.github.com>
2021-08-29 16:27:27 -0400

    st_feinit

--- src/libmld/st_feinit.c
+++ src/libmld/st_feinit.c
@@ -1,24 +1,3 @@
-__asm__(R""(
-.macro glabel label
-    .global \label
-    .balign 4
-    \label:
-.endm
+void st_feinit(void) {
 
-
-
-
-.set noat      # allow manual use of $at
-.set noreorder # don't insert nops after branches
-
-.text
-
-glabel st_feinit
-    .ent st_feinit
-    # 0048AE84 st_filebegin
-/* 00439BC4 03E00008 */  jr    $ra
-/* 00439BC8 00000000 */   nop   
-    .type st_feinit, @function
-    .size st_feinit, .-st_feinit
-    .end st_feinit
-)"");
+}
